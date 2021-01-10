a1cd87fc96b86e47dbd5b57545a9a8ecd902f260
Sita <sitavemuri8@gmail.com>
2021-01-09 22:00:31 -0800

    Add descriptions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function createCards() {
     let cardtitles = ["First Past the Post", "Two Round Runoff", "Instant Runoff/Alternative Vote", "Borda Count", "Condorcet Method", "Approval Voting", "Proportional Representation", "Score/Cardinal Voting"];
-    let cardtext = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
     for (i = 0; i < cardtitles.length; i++) {
         let card = document.createElement('div');
         card.className = "card";
+        card.id = `${i}`
         grid = document.getElementById('grid');
         grid.appendChild(card);
 
@@ -12,8 +12,15 @@ function createCards() {
         title.innerHTML = `${cardtitles[i]}`;
         card.appendChild(title);
 
-        let text = document.createElement('p');
-        text.innerHTML = `${cardtext[i]}`;
-        card.appendChild(text);
+        card.addEventListener('mouseover', () => {
+            showInfo(card.id);
+        });
     }
+}
+
+function showInfo(index) {
+    let cardtext = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let info = document.getElementById("info-p");
+    info.innerHTML = `${cardtext[index]}`;
+    info.style.display = "inherit";
 }
